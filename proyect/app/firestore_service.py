2e4a740de3e4dab3faba0f8b0ad4fd09e85eba5a
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2022-06-22 22:02:52 -0500

    Added get and display of to_dos and to_do_lists

--- proyect/app/firestore_service.py
+++ proyect/app/firestore_service.py
@@ -7,5 +7,23 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-def get_users():
+def get_users_dicts ():
     return [user.to_dict() for user in db.collection("users").get()]
+
+def get_users ():
+    return [user for user in db.collection("users").get()]
+
+def get_to_do_lists_dicts ():
+    return [to_do_list.to_dict() for to_do_list in db.collection("to_do_lists").get()]
+
+def get_to_do_lists ():
+    return [to_do_list for to_do_list in db.collection("to_do_lists").get()]
+
+def get_to_dos (to_do_list_id):
+    return [to_do.to_dict() for to_do in \
+            db.collection("to_do_lists")\
+            .document(to_do_list_id)\
+            .collection("to_do")\
+            .get()
+            ]
+
--- proyect/main.py
+++ proyect/main.py
@@ -2,17 +2,22 @@
 from flask import render_template, session
 
 from app import create_app
-from app.firestore_service import get_users
+from app.firestore_service import get_users_dicts, get_to_do_lists_dicts, get_to_do_lists, get_to_dos
 
 app = create_app()
 
 @app.route("/")
 def index ():
     user_email = session.get("u_email")
+    
+    to_do_lists = get_to_do_lists()
+    to_dos = [get_to_dos(to_do_list.id) for to_do_list in to_do_lists]
 
     context = {
             "u_email": user_email,
-            "users": get_users()
+            "users": get_users_dicts(),
+            "to_do_lists": get_to_do_lists_dicts(),
+            "to_dos": to_dos
             }
 
     return render_template("index.html", **context)
